6a172f505ade1bb902de37b4c1872aba0ee395df
Kedwin Hernandez <tokenandfriends@gmail.com>
2017-07-08 17:06:04 -0400

    created new comment component

---
+++ src/Comment.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import style from './css/style';
+import marked from 'marked';
+
+class Comment extends Component {
+  rawMarkup(){
+    let rawMarkup = marked(this.props.children.toString());
+    return {_html: rawMarkup};
+  }
+  render(){
+    return(
+      <div style={style.comment}>
+        <h3>{this.props.author}</h3>
+        <span dangerouslySetInnerHTML={this.rawMarkup()}></span>
+      </div>
+    );
+  }
+}
+
+export default Comment;
